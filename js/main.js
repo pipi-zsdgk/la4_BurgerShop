ca31a11daf2a7eaa6fdf1347186e3730f3f047c2
{pipi-zsdgk} <{sklgm122510@gmail.con}>
2022-09-01 00:15:40 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,53 @@
+$(function($){
+    $('.bg-swicher').bgSwitcher({
+        images: ['images/bg1.jpg','images/bg2.jpg','images/bg3.jpg'],
+        interval: 5000,
+        loop: true
+    });
+
+
+    $(window).scroll(function (){
+        var targetElement = $('.fadein').offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if(scroll > targetElement - windowHeight + 200){
+            $('.fadein').css('opacity','1');
+            $('.fadein').css('transform','translateY(0)');
+        }
+    });
+
+    $('.slider').slick({  //class名sliderの要素に対してslickを実行
+        arrows: false,   //左右矢印を表示しない
+        autoplay: true,  //自動再生をする
+        dots: true,  //ドットを表示する
+        centerMode: true, //センタ―モード(両端見切れ状態)になる
+        centerPadding: '20%', //センターモードｎ時左右のスライドを20%ずつ表示する
+    });
+
+    $('.more-button').on('click', function(){
+        if($('.open-box').is(':hidden')){
+            $('.open-box').slideDown();
+            $(this).text('close');
+            $(this).css('background-color', '#808080');
+        } else {
+            $('.open-box').slideUp();
+            $(this).text('more');
+            $(this).css('background-color', '#fabb51');
+        }
+    });
+
+    $(".modal-button").modaal();
+
+    $('nav a[href^="#"]').click(function(){
+        var adjust = 0;
+        var speed = 400;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - adjust;
+        $('body,html').animate({
+            scrollTop: position
+        }, speed, 'swing');
+        return false;
+    });
+
+});
